c273085e02920425ba0abf7e15dcb0b64aece2d2
dilkhush05 <dilkhush.thakur05gmail.com>
2025-08-22 18:46:46 +0530

    #

--- LinkedListImpQeu.java
+++ LinkedListImpQeu.java
@@ -1,4 +1,5 @@
 
+import java.net.SocketPermission;
 import java.util.*;
 
 public class LinkedListImpQeu {
@@ -57,16 +58,91 @@ public class LinkedListImpQeu {
 
     }
 
+
+    // _______ReverseNthLLfromLast_______________
+
+    public static void ReverseNthLLfromLast(int n){
+        int sz = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+
+        if(sz == n){
+            head = head.next;
+            return;
+        }
+
+        Node prev = head;
+        int szRevNo = sz- n;
+        int i =1;
+        while(i < szRevNo){
+            prev = prev.next;
+            i++;
+        }
+
+        prev.next = prev.next.next;
+        return;
+        
+    }
+
+    //____________FindMidOfAnyLL______________________
+    public Node FindMidOfAnyLL(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+
+    }
+    // _______________CheckPalonDrome_________________
+
+    public Boolean CheckPalonDrome(){
+        //normal thinking 
+        if(head == null || head.next == null )return true;
+        
+        Node midNode = FindMidOfAnyLL(head);
+
+        Node prev = null;
+        Node curr = midNode;
+        Node next ;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next; 
+        }
+
+        Node left = prev;
+        Node right = head;
+        while(right != null){
+            if(left.data != right.data){
+                return false ;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+
+    }
+
     public static void main(String[] args) {
         LinkedListImpQeu LL = new LinkedListImpQeu();
         LL.AddFirst(1);
         LL.AddFirst(2);
-        LL.AddFirst(3);
-        LL.AddFirst(4);
-        LL.AddFirst(5);
+        LL.AddFirst(2);
+        LL.AddFirst(1);
+        // LL.AddFirst(5);
         
         LL.Print();
-        LL.ReverseLL();
+        // LL.ReverseLL();
+        // ReverseNthLLfromLast(2);
+        System.out.println(LL.CheckPalonDrome());
         LL.Print();
     }
 }
